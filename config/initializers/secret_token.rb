9fca52276cb50b1cc3abd9e4411c1dc26e8f071e
Shane Becker <veganstraightedge@gmail.com>
2011-05-13 15:57:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EMRubyConf::Application.config.secret_token = 'e821db93380d666c3fbce0b724d5b64c5832b6a955ebc9a6626d5e0744f40888ff5059e33b0030c369b38d5fb0abf7d85282a05f39b4a8c6c4e9a7bfcf79cd21'
